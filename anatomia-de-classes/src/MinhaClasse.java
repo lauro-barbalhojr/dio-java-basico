8639fe5a0b3d0a899f6efb6408b9e8dc94f34fc5
Lauro <lauro.abj@gmail.com>
2023-12-19 15:45:03 -0300

    First Hello world

---
+++ anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse {
+    /*Corpo da classe, que é um arquivo criado no código fonte,
+    o diretório src do porjeto.
+     */
+    public static void main(String[] args) {
+        /*
+        Corpo do método main, que tem um parâmetro String, e o parâmetro
+        [] que indica um array. Esse é um método específico dentro de uma classe,
+        que conterá todas as operações dentro de seu escopo.
+         */
+        System.out.print("Hello World!");
+        /*
+        System é uma classe que tem inúmeras operações, nesse caso, a operação
+        escolhida foi a out, operação de saída para  uma impressão, com um parâmetro
+        sendo um texto em forma de string.
+         */
+
+    }
+}
